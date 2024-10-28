7841095b14aae35be5a952bf2a745298f0820ae3
AFHenriquezB07 <afhenriquez@ufpso.edu.co>
2024-10-28 10:06:42 -0500

    funcion lambda Consumer

--- src/main/java/AF/estudio/lambdas/consumer/ConsumerExample.java
+++ src/main/java/AF/estudio/lambdas/consumer/ConsumerExample.java
@@ -12,9 +12,18 @@ public class ConsumerExample {
         Recibe un valor y no retorna nada.
         */
 
+        // Manera de utilizar la funcion lambda
+        /*
         Consumer<String> consumer = (String param) -> {
             System.out.println(param);
         };
+        */
+
+        /*
+        Consumer<String> consumer = (String param) -> System.out.println(param);
+        */
+
+        Consumer<String> consumer = System.out::println;
 
         consumer.accept("Andres Felipe Henriquez Bayona");
 
